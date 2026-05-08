f526c337cefe6e839512735bb0831ebe6a6d463d
Nathanael Wong <natgeo.wong@outlook.com>
2026-05-08 04:28:04 -0400

    Updates to GOES now we have a package

--- scripts/goes.jl
+++ scripts/goes.jl
@@ -1,32 +1,13 @@
 using DrWatson
 @quickactivate "STRIde"
 
-using Dates
-using Logging
-using Printf
-using AWS, AWSS3
+using GOES
 
-aws = AWSConfig(; creds=nothing, region="us-east-1")
+sID = parse(Int, ARGS[1])
+yr  = parse(Int, ARGS[2])
+mo  = parse(Int, ARGS[3])
+gds = GOESDataset(ID=sID,product="ABI-L2-ACMC",path=datadir())
+geo = GeoRegion("SGP_LARGE",path=srcdir())
+gvar = "BCM"
 
-bucket = "noaa-goes16"
-product = "ABI-L2-ACMC"   # Cloud & Moisture Imagery - Full Disk
-year = 2020
-
-for day in 180 : 187, hour in 0 : 23
-
-    prefix = "$product/$year/$(@sprintf("%03d", day))/$(@sprintf("%02d", hour))/"
-    ymd = Dates.format(Date(year,1,1) + Day(day - 1),dateformat"yyyymmdd")
-    ymdh = "$(ymd)$(@sprintf("%02d", hour))"
-    mkpath(datadir("goes",product,ymdh))
-
-    for (ii, obj) in enumerate(keys)
-        s3_get_file(
-            aws, bucket, obj["Key"],
-            datadir(
-                "goes",product,ymdh,
-                "time$(@sprintf("%02d", ii)).nc"
-            )
-        )
-    end
-
-end
+download(gds,geo,gvar,start=Date(yr,mo),stop=Date(yr,mo,daysinmonth(yr,mo)),NT=UInt8)
